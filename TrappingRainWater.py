3796c3b4addc20448c2d7defa790790f6f4ba792
SKeerthi02 <57923817+SKeerthi02@users.noreply.github.com>
2020-08-31 17:46:33 -0400

    day 8
    Stacks: Hard question.

---
+++ TrappingRainWater.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0 
+        right = len(height) - 1
+        
+        maxleft, maxright, ans = 0, 0, 0
+        
+        while left < right:
+            
+            if height[left] < height[right]:
+                
+                if height[left] >= maxleft:
+                    maxleft = height[left]
+                else:
+                    ans += maxleft - height[left]
+                left += 1    
+            else:
+                if height[right] >= maxright:
+                    maxright = height[right]
+                else:
+                    ans += maxright - height[right]
+                right -= 1
+            
+        return ans
