19a166c252c125d2ccf79d7c6d8d02aad66e0b11
SKeerthi02 <57923817+SKeerthi02@users.noreply.github.com>
2020-09-04 07:38:33 -0400

    Day 11
    bit Manipulation.

---
+++ NoStepsToResuceaNumbertoZero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        num = bin(num)[2:]
+        count = 0
+        for n in num:
+            if n == "1":
+                count += 2
+            else:
+                count += 1
+        return count - 1
